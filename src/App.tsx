1d02fdef1823f19589175cc3a4ffe9c81aea7761
Volodymyr Pshoniak <volodymyr.pshoniak@stuzo.com>
2022-08-23 11:04:40 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.module.css'
+import { Unit } from './components/Unit/Unit'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div>
+        <h4>Title</h4>
+        <div>
+          <Unit iconName="face" title="Hi" description="description" />
+          <Unit iconName="face" title="Hi" description="description" />
+          <Unit iconName="face" title="Hi" description="description" />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/Unit/Unit.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Unit.module.css'
+
+interface IUnitProps {
+  iconName: string
+  title: string
+  description: string
+}
+
+export const Unit: React.FC<IUnitProps> = ({ iconName, title, description }) => {
+  return (
+    <div className={styles.unitWrapper}>
+      <span className="material-icons">{iconName}</span>
+      <span>{title}</span>
+      <span>{description}</span>
+    </div>
+  )
+}
